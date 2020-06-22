a20e044805d39c3f341710f9ed93e3bf0e30c29d
Sergio Rodriguez <34104196+srodriguez9017@users.noreply.github.com>
2020-06-22 16:09:02 -0500

    Update SolObservador.java

--- Observer/src/main/java/com/mitocode/observer/SolObservador.java
+++ Observer/src/main/java/com/mitocode/observer/SolObservador.java
@@ -5,10 +5,13 @@ public class SolObservador extends Observador{
 	private double valorCambio = 3.25;
 	
 	public SolObservador(Subject sujeto) {
+		//ESTOS THIS HACEN REFERENCIA  A LA PROPPIEDAD DE LA CLASE  ABSTRACTA
 		this.sujeto = sujeto;
+		//SE AGREGAEL SUJETO AL ARRAY DE OBSERVADORES
 		this.sujeto.agregar(this);
 	}
 	
+	//SIMPLEMENTESE SOBRE ESCRIBE EL METODO ACTUALIZAR DE LA CLASE ABSTRACTA
 	@Override
 	public void actualizar() {		
 		System.out.println("PEN: " + (sujeto.getEstado() * valorCambio));
